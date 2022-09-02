228dbff5fad271df0d763a6214c773337d939857
Egrenbido Oyintare Paul <73933865+OyintarePaul@users.noreply.github.com>
2022-09-02 14:18:18 +0100

    Create news.js

---
+++ data/news.js
@@ -0,0 +1,161 @@
+const newsArticles = [
+
+	{		title: "Barcelona 3-3 Man City highlights and reaction as Riyad Mahrez rescues dramatic late draw",
+
+		description: "3-3 is how the match ends! As friendlies go that was a pretty entertaining one. City took the lead through a rather fortuatous Julian Alvarez goal, before Pierre-Emerick Aubameyang drew Barcelona level. Memphis Depay strikes either side of a Cole Palmer tap-in looked to have sealed the win for Barca, but an emphatic late penalty from Riyad Mahrez ensured that the game finished all-square. City's players got the run out that Guardiola wanted, but there will be an anxious wait to find out the severity of Luke Mbete's head injury. You can have your say on how the City players got on by using our player ratings widget below.",
+
+		coverUrl: "https://i2-prod.manchestereveningnews.co.uk/incoming/article24845835.ece/ALTERNATES/s1200/0_Blog-for-Barcelona.jpg",
+
+		views: 567,
+
+		comments: 118,
+
+		date: "2022-10-8"
+
+	},
+
+	{
+
+		title: "Maupay undergoing Everton medical to face Brentford",
+
+		description: "Everton are hopeful a deal can be struck in time for him to face his former club Brentford on Saturday but there is still some work to do to complete negotiations before midday on Friday. The Toffees have already shown an interest in Southampton's Che Adams this week. One source has told Sky Sports the deal under discussion would value Adams at up to £25m. Maupay scored eight goals for the Seagulls last season, but is yet to start a league game for the club this term with Danny Welbeck favoured in attack." ,
+
+		coverUrl: "https://e0.365dm.com/22/08/768x432/skysports-neal-maupay-brighton_5876258.jpg?20220825220432",
+
+		views: 480,
+
+		comments: 150,
+
+		date: "2022-10-8"
+
+	},
+
+	{
+
+		title: "The Europa League and Europa Conference League group-stage draws.",
+
+		description: "The 2022/23 UEFA Europa League group stage draw will take place from 12pm (BST) on Friday, August 26. The draw ceremony is being held in Istanbul, Turkey. The final 10 teams in the group stages, finishing off all 32 confirmed sides, were decided with the second leg of the play-off qualification round on Thursday evening. The 32 clubs are seeded into four pots of eight teams, with the club coefficient rankings established at the beginning of the season.",
+
+		coverUrl: "https://e0.365dm.com/22/07/768x432/skysports-jose-mourinho-roma_5837612.jpg?20220717202521",
+
+		views: 600,
+
+		comments: 124,
+
+		date: "2022-10-8"
+
+	},
+
+	{
+
+		title: "Champions League draw: Liverpool to meet Rangers and Celtic face Real Madrid",
+
+		description: "Liverpool and Rangers will meet in the Champions League group stage, while Manchester City's Erling Haaland is set to face his old club Borussia Dortmund. Scottish champions Celtic are in the same group as holders Real Madrid and Chelsea face AC Milan. Eintracht Frankfurt, who are making their group stage debut, are in the same group as Tottenham. Bayern Munich, Barcelona and Inter Milan have been drawn in the same group.",
+
+		coverUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx8lQtAiFoFH8MvEqXqaGUQS_AF-1Gi_qlCw&usqp=CAU",
+
+		views: 450,
+
+		comments: 120,
+
+		date: "2022-10-8"
+
+	},
+
+	{
+
+		title: "Aberdeen vs Rangers Scottish Premiership clash to be shown live on Sky Sports",
+
+		description: "Giovanni van Bronckhorst's side will visit Pittodrie at noon on Saturday September 10, following the first Old Firm of the season which is also live on Sky Sports. The Dons drew the opening two meetings between the sides last season before losing the most recent clash at Ibrox and will be aiming to record their first league win over Rangers since 2018. Prior to that, the Sky Sports cameras will be at Tannadice Park as Ange Postecoglou's side return to the ground they sealed the title last season. The first Old Firm of the new season then takes place on September 10, live on Sky Sports, as Rangers visit Celtic in what will no doubt be a key fixture in the title race.",
+
+		coverUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH1Azfrc4vTOGNLdZh1-FoOY1vd1gGhyKLTw&usqp=CAU",
+
+		views: 150,
+
+		comments: 36,
+
+		date: "2022-10-8"
+
+	},
+
+	{
+
+		title: "Man Utd transfer news LIVE: Antony to join for £84m EXCLUSIVE, Ten Hag’s Cristiano Ronaldo AXE, Sergino Dest wanted",
+
+		description: "The Brazilian's representatives are now reportedly staying in England until the end of the transfer window to push through a deal. Meanwhile, Cristiano Ronaldo was brutally axed in front of the whole Manchester United squad during a two-hour summit meeting, SunSport reveals. And United are said to be ready to go back in for Sergino Dest of Barcelona, as they look for a right-back to replace Aaron Wan-Bissaka. The Dutch-American, who worked with Erik ten Hag at Ajax, is said to consider Old Trafford to be a dream move.",
+
+		coverUrl: "https://ic-cdn.flipboard.com/thesun.co.uk/18895b02099445327618505abdaedf642c4047d7/_xlarge.webp",
+
+		views: 611,
+
+		comments: 199,
+
+		date: "2022-10-8"
+
+	},
+
+	{
+
+		title: "Erik ten Hag dropped off at training by wife as Man Utd boss aims to make it back-to-back Premier League wins",
+
+		description: "The result relieved some of the pressure which was already building following back-to-back losses to Brighton and Brentford at the start of the campaign. Now he is seeking to build on that impressive display against Jurgen Klopp's men when United travel to St Mary's to take on Southampton on Saturday lunchtime. And Ten Hag could be seen being dropped off by his partner Bianca just 24 hours before the clash on the south coast. United stars Jadon Sancho and Anthony Martial were also spotted arriving at the club's Carrington training ground.",
+
+		coverUrl: "https://www.thesun.co.uk/wp-content/uploads/2022/08/TC-SPORT-UTD-CARS-1.jpg?w=1320",
+
+		views: 331,
+
+		comments: 185,
+
+		date: "2022-10-8"
+
+	},
+
+	{
+
+		title: "Ronaldo's agent makes Napoli approach over potential move",
+
+		description: "Nigeria striker Victor Osimhen has been discussed as part of a deal involving Cristiano Ronaldo heading to Napoli; Manchester United forward's representative Jorge Mendes has approached Napoli; Osimhen is a player admired at United and on their list of possible replacements. Cristiano Ronaldo's representative Jorge Mendes has approached Napoli over a potential move.Mendes is working hard to find Ronaldo the solution he wants - to play in the Champions League - but also to provide a solution for United, which is a replacement. Napoli, however, would only accept a triple-digit offer for Osimhen and it is difficult to see how United could afford him with the money they are prepared to commit to buying Antony from Ajax.",
+
+		coverUrl: "https://e0.365dm.com/22/08/768x432/skysports-cristiano-ronaldo_5868292.jpg?20220818102338",
+
+		views: 109,
+
+		comments: 92,
+
+		date: "2022-10-8"
+
+	},
+
+	{
+
+		title: "Liverpool draw Rangers in CL; Haaland set for Dortmund reunion",
+
+		description: "Liverpool and Rangers set for Champions League group stage battle of Britain; Erling Haaland set for Borussia Dortmund reunion; Chelsea draw Milan, Salzburg and Zagreb in Group E; Spurs handed favourable Frankfurt, Sporting, Marseille draw; Celtic drawn with holders Real Madrid in Group F. The Scottish side, who overcame PSV on Wednesday to qualify for the group stages, will face the Reds for the first time in a competitive match, as well as group games against Dutch champions Ajax and Italian side Napoli. Erling Haaland will get a speedy reunion with former club Borussia Dortmund after Manchester City, last season's beaten semi-finalists, were drawn against the German club, Sevilla and FC Copenhagen in Group G. Chelsea will come up against former defender Fikayo Tomori after the 2021 winners were paired with AC Milan in Group E. Thomas Tuchel's side will also face Salzburg and Dinamo Zagreb and will be favourites to progress to the knockout stage.",
+
+		coverUrl: "https://e0.365dm.com/22/08/768x432/skysports-salah-tavernier-rangers_5876052.jpg?20220825184416",
+
+		views: 101,
+
+		comments: 50,
+
+		date: "2022-10-8"
+
+	},
+
+	{
+
+		title: "Fantasy Premier League LIVE! GW4: Team news, injury updates and expert advice",
+
+		description: "In our latest column, Holly Shand, FPL Heisenberg and FPL Family - who between them have ten top 10k finishes and more than 30 years of Fantasy experience - discuss what their wildcard teams would look like and who their captain is for the week. James Ward-Prowse (£6.5m) has blanked in only one of the first three GWs, scoring against Spurs and supplying an assist against Leicester to total 18 points. If you are looking to bring in a goalkeeper, Brighton's Robert Sanchez (£4.5m) is my preference because they have a solid defence and a favourable run of fixtures. Another good cheap goalkeeper with favourable fixtures is Brentford's David Raya (£4.5m). : I too have Sanchez and I'm thinking of sticking with him. Brighton have been excellent defensively so far this season, only conceding one goal. Only Man City and Arsenal have a better xGC (expected goals conceded), which means their defensive performances so far - and the FPL points Sanchez is getting - should continue, assuming they keep it up.",
+
+		coverUrl: "https://e0.365dm.com/22/08/768x432/skysports-fantasy-premier-league_5875562.jpg?20220825120310",
+
+		views: 81,
+
+		comments: 43,
+
+		date: "22-10-8"
+
+	}
+
+];
